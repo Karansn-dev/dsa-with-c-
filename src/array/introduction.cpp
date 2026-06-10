9835b7a8b204fdd0c3e9ad559798e1f662bdc008
Karan Singh Negi <karan.sn2004@gmail.com>
2026-06-10 22:18:43 +0530

    array new question

---
+++ src/array/introduction.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector<int> arr;
+    int n ;
+    cout<<"enter the size of the array = ";
+    cin>> n;
+
+     
+    vector<int> v2(3, 5);  
+     
+    
+    // for(int i = 0; i <n ; i++){
+    //     arr[i] = i+1;
+    // }
+
+    // arr.pop_back();
+
+    // for(int i =0; i<n; i++){
+    // cout<< arr[i] << " ";
+     
+    for(int i =0; i<n; i++){
+    cout<< v2[i] << " ";
+}
+}   
+            
